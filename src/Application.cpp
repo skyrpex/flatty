9ea4622fc669bfb8e00a8fba551aa8f7f61b5fbf
Cristian Pallarés <skyrpex@gmail.com>
2013-05-22 21:31:28 +0200

    Use Fusion style

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,4 +1,6 @@
 #include "Application.h"
+#include <QStyleFactory>
+#include <QPalette>
 
 Application::Application(int argc, char **argv) :
     QApplication(argc, argv),
@@ -7,6 +9,24 @@ Application::Application(int argc, char **argv) :
     setOrganizationName("VGAFIB");
     setApplicationName("Flatty");
     setApplicationVersion("0.0");
+
+    setStyle(QStyleFactory::create("fusion"));
+
+//    QPalette palette;
+//    palette.setColor(QPalette::Window, QColor(53,53,53));
+//    palette.setColor(QPalette::WindowText, Qt::white);
+//    palette.setColor(QPalette::Base, QColor(15,15,15));
+//    palette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+//    palette.setColor(QPalette::ToolTipBase, Qt::white);
+//    palette.setColor(QPalette::ToolTipText, Qt::white);
+//    palette.setColor(QPalette::Text, Qt::white);
+//    palette.setColor(QPalette::Button, QColor(53,53,53));
+//    palette.setColor(QPalette::ButtonText, Qt::white);
+//    palette.setColor(QPalette::BrightText, Qt::red);
+
+//    palette.setColor(QPalette::Highlight, QColor(142,45,197).lighter());
+//    palette.setColor(QPalette::HighlightedText, Qt::black);
+//    setPalette(palette);
 }
 
 QUndoStack *Application::undoStack()
